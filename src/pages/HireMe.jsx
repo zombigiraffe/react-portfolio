8ca98dd6ef5e4a5ac94da8e51c9d5b8c1d8c485f
Arron Mixell <arronmixell@yahoo.com>
2022-08-14 05:26:01 -0400

    you think you're done but you never are

--- src/pages/HireMe.jsx
+++ src/pages/HireMe.jsx
@@ -7,9 +7,9 @@ export function HireMe() {
       <h1>Let's talk...</h1>
       <h2>You can reach me at:</h2>
       <a class="mail-to" href="mailto:arronmixelldev@gmail.com">arronmixelldev@gmail.com</a>
-      <h2 className="hire-me-h2">Or on my LinkedIn.<a className="hire-me-linkedin" href="https://www.linkedin.com/in/arron-mixell-30b872155/">
+      <a className="hire-me-linkedin" href="https://www.linkedin.com/in/arron-mixell-30b872155/">
           <LinkedinIcon />
-        </a></h2>
+        </a><h2 className="hire-me-h2">Or on my LinkedIn.</h2>
       
       <h3 className="hire-me-h3">Thank you.</h3>
       <img className="worker" src={images.worker} alt="worker-img"/>
