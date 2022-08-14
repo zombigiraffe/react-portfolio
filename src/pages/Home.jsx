68c5ac696b9ae0304e8d0e30a58221de52991b14
Arron Mixell <arronmixell@yahoo.com>
2022-08-14 01:02:09 -0400

    added majority of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import LinkedinIcon from "../components/Linkedin-Icon";
+
 export function Home() {
   return (
-    <div className="home-div">
-      <h3 className="home-h3">Hi, my name is</h3>
-      <h1>Arron Mixell.</h1>
-      <h4 className="home-h1">A web developer.</h4>
-      <h2>Thank you for visiting and learning a little about me.</h2>
+    <div>
+      <div className="home-div">
+        <h3 className="home-h3">Hi, my name is</h3>
+        <h1>Arron Mixell.</h1>
+        <h4 className="home-h1">A web developer.</h4>
+        <h2>Thank you for visiting and learning a little about me.</h2>
+        <a href="https://www.linkedin.com/in/arron-mixell-30b872155/">
+          <LinkedinIcon />
+        </a>
+      <div className="cv">
+        <a href="../public/Arron-Mixell.pdf" download>
+          Download CV
+        </a>
+      </div>
+      </div>
     </div>
   );
 }
